e6dcec4a0d19dddfe3596746849f8e2521de62ff
nostalgia2021 <nostalgia092@yahoo.com>
2022-02-15 17:50:08 -0800

    completed

---
+++ script.js
@@ -0,0 +1,21 @@
+const pw = document.querySelector("#password");
+const confirmPW = document.querySelector("#confirm-password");
+const form = document.querySelector("form");
+const warningMsg = document.querySelector('#warningMsg');
+const submit = document.querySelector(".submit");
+
+const validatePassword = function (e) {
+
+    if (pw.value !== confirmPW.value && pw.value.length >= 7) {
+        e.preventDefault();
+        pw.setCustomValidity("Passwords do not match!");
+        pw.reportValidity();
+        // console.log("password do not match", pw.value, confirmPW.value)
+        // warningMsg.textContent = "* Passwords do not match!";
+    } else {
+        pw.setCustomValidity("");
+        // form.submit();
+    }
+}
+
+submit.addEventListener('click', validatePassword);
